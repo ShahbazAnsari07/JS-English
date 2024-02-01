4f9ba6093393cfb25f5be0f9ee0b87eeb357dbc9
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-01 23:53:17 +0530

    Update 03_conversionOperation.js

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,3 +25,32 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 4
+let negValue  = -value;
+// console.log(negValue);
+
+let str1 = "hello "
+let str2 = "shahbaz"
+
+let str3 = str1 +str2;
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
