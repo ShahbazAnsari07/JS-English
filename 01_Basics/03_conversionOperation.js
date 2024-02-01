95acb0222a23455247dcfea4c0ed53a38d301054
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-01 18:00:22 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof  valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true  => 1; false => 0
+
+let isLoggedIn = "Shahbaz";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "shahbaz" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
