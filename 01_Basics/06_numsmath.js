5ae9750b5307a6a12aa3b0da612c80395264d92e
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-06 17:57:16 +0000

    maths

--- 01_Basics/06_numsmath.js
+++ 01_Basics/06_numsmath.js
@@ -17,6 +17,20 @@ const hundreds = 1000000
 
 // ++++++++++++++ Maths +++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-9));
-console.log(Math.round(14.668));
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(14.668));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 32, 1, 2));
+// console.log(Math.max(22, 10, 15, 36));
+
+console.log(Math.random());     // values lie only between 0 and 1.
+
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
