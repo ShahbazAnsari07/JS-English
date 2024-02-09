46cf78f85d9064b5474e97a5695fdf42aaf993b9
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-09 18:09:33 +0000

    objects

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton
+
+// object literals
+
+const JsUser = {
+    name: "Shahbaz",
+    age: 21,
+    location: "Delhi",
+    email: "shahbaz@google.com",
+    isLoggedIn: false,
+    lastLogingDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
