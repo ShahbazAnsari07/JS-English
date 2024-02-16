a99c2cb6123f2c95e5f0e9b82a5f274877da1aa4
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-16 17:44:44 +0000

    Edit

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -2,8 +2,13 @@
 
 // object literals
 
+const mySym = Symbol("key1")
+
+
 const JsUser = {
     name: "Shahbaz",
+    "full name": "Shahbaz Ansari",
+    [mySym]: "mykey1",
     age: 21,
     location: "Delhi",
     email: "shahbaz@google.com",
@@ -11,5 +16,24 @@ const JsUser = {
     lastLogingDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+
+JsUser.email = "shahbaz@chatgpt.com"
+// Object.freeze(JsUser);
+JsUser.email = "shahbaz@microsoft.com";
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS Programmer");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js Programmer, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
