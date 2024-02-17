be459cf0bd28c36be12b103d121af37c90e3342f
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-17 17:54:59 +0000

    object2

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -22,5 +22,34 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 
-const obj3 = { obj1, obj2 }
-console.log(obj3);
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+
+]
+
+users[1].email
+console.log(tinderUser);
+
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
