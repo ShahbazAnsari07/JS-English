b136019e4971eede101fe87ee4d7b8e009384019
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-02-18 18:23:33 +0000

    updatefunctions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -28,11 +28,40 @@ const result = addTwoNumbers(3, 4)
 
 function loginUserMessage(username = "Sam"){
     if(!username){
-        console.log("Please enter a username");
+        // console.log("Please enter a username");
         return
     }
     return `${username} just logged in`
 }
 
 // console.log(loginUserMessage("Shahbaz"))
-console.log(loginUserMessage("shahbaz"));
+// console.log(loginUserMessage("shahbaz"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "shahbaz",
+    price: 200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const newArray = [20, 40, 50, 60]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+console.log(returnSecondValue(newArray));
