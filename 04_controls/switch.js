9072de53c2cd63cf733f96bd094a2be710c09d7d
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-03-21 12:09:25 +0000

    switch

---
+++ 04_controls/switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "March"
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
