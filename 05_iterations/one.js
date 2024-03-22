e9227ae1d8728ab934ebaf64660fddd261e01b4b
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-03-22 07:06:46 +0000

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,22 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is the best number");
+    }
+    // console.log(element);
+
+}
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+        console.log(i + '*' + j + '=' + i*j);
+        
+    }
+
+}
+
