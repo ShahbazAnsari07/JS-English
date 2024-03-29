f82cb887d27c02c317e12028e3d49ec83cd8f06a
SHAHBAZ ANSARI <120576505+ShahbazAnsari07@users.noreply.github.com>
2024-03-29 19:16:05 +0000

    six

---
+++ 05_iterations/six.js
@@ -0,0 +1,10 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// console.log(values);
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
